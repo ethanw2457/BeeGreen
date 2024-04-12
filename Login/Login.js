b14422b5ae135177819c2043c39954daf9e39f35
ethanw2457 <130408385+ethanw2457@users.noreply.github.com>
2024-04-12 21:38:42 +0000

    botched bee for login

--- Login/Login.js
+++ Login/Login.js
@@ -11,39 +11,6 @@ sign_in_btn.addEventListener('click', () =>{
 });
 
 
-// the below is not used but for reference
-document.getElementById("signupform").addEventListener("submit", function(event) {
-  event.preventDefault(); 
-
-  const name = document.getElementById("signupname").value.trim();
-  const email = document.getElementById("email").value.trim();
-  const password = document.getElementById("signuppassword").value.trim();
-  const address = document.getElementById("address").value.trim();
-
-  if (name === "" || email === "" || password === "" || address === "") {
-    alert("Please fill in all fields.");
-    return;
-  }
-  var i = 1;
-  while (localStorage.getItem("user" + i) !== null) {
-    i++;
-  }
-  localStorage.setItem("user" + i, name);
-  localStorage.setItem("email" + i, email);
-  localStorage.setItem("address" + i, address);
-  localStorage.setItem("password" + i, password);
-
-  localStorage.setItem("status" + i, "carpooler");
-
-  localStorage.setItem("currentuser", i);
- 
-  //document.getElementById("result").innerHTML = localStorage.getItem("user1");
-
-  //localStorage.clear();
-  // Assume AJAX call to send login info to server and save in database
-  // Redirect to another page after successful login
-  window.location.href = "Ranking/Ranking.html"; // Redirect to event selection page
-});
 
 document.getElementById("signinform").addEventListener("submit", function(event) {
   event.preventDefault(); 
